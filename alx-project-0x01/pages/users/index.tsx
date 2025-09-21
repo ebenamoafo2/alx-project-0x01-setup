b39b8035a8c60b547f44269196d5d16ce5eb57bb
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-21 00:06:45 +0000

    Refactor getStaticProps function to fetch user data from API and return as props

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -94,5 +94,16 @@ const Users: React.FC = () => {
     </div>
   );
 };
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 
 export default Users;
